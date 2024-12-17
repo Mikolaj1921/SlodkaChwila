d201e141664f498840685579203083e816179aa7
Mikolaj231 <mikolajmelnyk16@gmail.com>
2024-12-17 21:43:27 +0100

    Aktualizacja 4.1 - endpoint maps

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 // server/index.js
-
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const contactRoute = require('./routes/contact'); // Importowanie trasy kontaktowej
 
 const app = express();
@@ -14,19 +12,40 @@ const FRONTEND_URL = process.env.FRONTEND_URL || 'https://slodkachwila.netlify.a
 app.use(cors({
     origin: FRONTEND_URL, // Zezwala tylko na połączenia z frontendu
     methods: ["GET", "POST", "PUT", "DELETE"], // Dozwolone metody HTTP
-    credentials: true // Jeśli korzystasz z ciasteczek lub sesji
+    credentials: true // Jeśli korzysta się z ciasteczek lub sesji
 }));
 
 // Middleware do parsowania JSON
-app.use(express.json()); // Używamy wbudowanego express.json() zamiast body-parser
+app.use(express.json()); 
 
-// Testowy endpoint dla weryfikacji działania serwera
+//1) Testowy endpoint dla weryfikacji działania serwera
 app.get('/', (req, res) => {
     res.send('Backend działa poprawnie!');
 });
 
 // Trasa do kontaktu
 app.use('/contact', contactRoute);
+
+
+
+
+
+
+// Endpoint do zwracania współrzędnych
+app.get("/api/location", (req, res) => {
+    const location = {
+      latitude: 50.03656577942113, // szerokość geograficzna
+      longitude: 21.99834561570134, // długość geograficzna
+      name: "Cukiernia Słodka Chwila",
+    };
+    res.json(location);
+});
+
+
+
+
+
+
 
 // Logowanie żądań (opcjonalne, ale przydatne do debugowania)
 app.use((req, res, next) => {
